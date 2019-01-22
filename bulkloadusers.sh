6062f87bf07829d4d25fd88b5404f4995760351a
Kaitlin Newson <kaitlin.newson@gmail.com>
2019-01-22 15:40:17 -0500

    Add bulkloadusers.sh

---
+++ bulkloadusers.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# A small script for bulk uploading users to Dataverse
+# using the API. For API documentation see:
+# http://guides.dataverse.org/en/latest/api/native-api.html#builtin-users
+
+host="" # Dataverse host URL
+temppass="" # Temporary password to set for users
+builtinkey="" # Built in users key - see the API guide to generate
+jsondir="" # Directory where the json files are stored
+
+for i in $jsondir/*;
+do
+    curl -d @"$i" -H "Content-type:application/json" "$host/api/builtin-users?password=$temppass&key=$builtinkey"
+done
